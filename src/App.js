a4096007bfccee3ca7d7a47f14a72a23f53035ac
Owen <owenizedd@gmail.com>
2019-10-10 23:25:06 +0700

    Combine All Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.css';
+import AppSearch from './LiveSearch/AppSearch';
+import App2 from './TicTacToe/App2';
 import Header from './Header';
 import MainContent from './MainContent'
-function App() {
-  return (
-    <div className="App">
-      <Header item1="Haha" item2="Hehe"/>
-      <MainContent />
-    </div>
-  );
+class App extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <AppSearch/>
+        <App2/>
+
+        {/* <div className="App meme-generator">
+          <Header item1="Haha" item2="Hehe"/>
+          <MainContent />
+        </div> */}
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/LiveSearch/AppSearch.js
+++ src/LiveSearch/AppSearch.js
@@ -1,53 +1,65 @@
 import React, {Component} from 'react';
 import './App.css'
 
-class App extends Component{
+class AppSearch extends Component{
   state={
     search: "",
     profiles: [],
+    cards: [],
   }
   handleChange = (evt) =>{
     // console.log("test")
+
+    const cards = this.state.profiles.map( (profile, index) => {
+      if ((profile.name.first + " " +  profile.name.last).toLowerCase().indexOf(evt.target.value.toLowerCase()) != -1){
+        return <div className="box" key={index}><img src={profile.picture.medium}/><p>{profile.name.first + " " + profile.name.last}</p></div>
+      }
+    })
+
     this.setState({
-      search: evt.target.value
+      search: evt.target.value,
+      cards: cards
     })
   }
 
   componentDidMount(){
-    for(let i = 0; i<10; i++){
+    for(let i = 0; i<1000; i++){
+      if (this.state.profiles.length>10) continue;
       fetch('https://randomuser.me/api/')
         .then(response => response.json())
         .then(data => {
-          console.log(data.results);
-          const profiles = data.results.map((profile,index) => {
-            return (<div className="box" key={index}>
-                    <img src={profile.picture.medium} width="100" height="auto"/>
-                    <p>{profile.name.first} {profile.name.last}</p>
-                  </div>)
-          })
-          // console.log(profiles);
-        
+          const profiles = data.results[0];
+
+          console.log(profiles);
           this.setState(prevState=>{
             let newProfiles = prevState.profiles;
-            console.log(newProfiles)
+            // console.log(newProfiles)
             newProfiles.push(profiles);
             return{
               profiles: newProfiles
             }
           })
         })
-        .finally(()=>{
-          console.log(i + " LAST")
-          console.log(this.state.profiles)
-        })
+    }
+  }
+
+  componentDidUpdate(){
+    if (this.state.profiles.length==10 && this.state.cards.length == 0){
+      const cards = this.state.profiles.map( (profile, index) => {
+        return <div className="box" key={index}><img src={profile.picture.medium}/><p>{profile.name.first + " " + profile.name.last}</p></div>
+      })
+      this.setState({
+        cards: cards
+      })
     }
   }
   render(){
     return(
       <div className="search-app">
+        <span>Input text to search user by name</span>
         <input type="text" name="search" id="" onChange={this.handleChange} value={this.state.search}/>
         <div className="boxes">
-          <div className="box">
+          {/* <div className="box">
             <img src="" alt=""/>
             <p>Name</p>
           </div>
@@ -66,14 +78,14 @@ class App extends Component{
           <div className="box">
             <img src="" alt=""/>
             <p>Name</p>
-          </div>
-          
+          </div> */}
+        
+        {this.state.cards}          
         </div>
 
-        {this.state.profiles}
       </div>
     )
   }
 }
 
-export default App;
+export default AppSearch;
